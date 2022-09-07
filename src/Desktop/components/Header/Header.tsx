938be167cd496887bb5671cb83f67d73fec96ed0
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2022-09-06 17:19:41 -0700

    remmove files

--- src/Desktop/components/Header/Header.tsx
+++ src/Desktop/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react'
 
 const HeaderPage: FunctionComponent = () => {
     return (
-        <a href="">
+        <a href="/">
             <img src={`${window.location.origin}/BLANCO UNAV NEW-NEW-08.png`} style={{ maxWidth: '180px' }} alt="" />
         </a>
     )
